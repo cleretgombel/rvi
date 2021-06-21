07bebb3fdfff79b37dbed1b41c20dcad1009e2e2
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-21 08:54:23 +0700

    Update runningtext.php

--- runningtext.php
+++ runningtext.php
@@ -1,1 +1,2 @@
-<marquee>TULISAN YANG AKAN BERGERAK</marquee>
+<marquee direction="left" scrollamount="10"> Tulisan berjalan ke kiri </marquee><br />
+<marquee direction="right" scrollamount="10"> Tulisan bejalan ke kanan </marquee>
