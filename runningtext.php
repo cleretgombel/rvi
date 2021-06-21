67dd3d4bae5587b2c8b0c4125bf222c210a359a3
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-21 08:56:44 +0700

    Update runningtext.php

--- runningtext.php
+++ runningtext.php
@@ -1,2 +1,16 @@
-<marquee direction="left" scrollamount="10"> Tulisan berjalan ke kiri </marquee><br />
-<marquee direction="right" scrollamount="10"> Tulisan bejalan ke kanan </marquee>
+<html>
+<head><title>Marquee Stop On Mouse Hover</title></head>
+<style type="text/css">
+.hakko {
+    background-color: #66232b;
+    color: #bebebe;
+    font-family: monospace;
+    font-size: 20px;
+    font-style: italic;
+}
+</style>
+<body>
+<marquee class="hakko" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();" > Marquee line berfungsi untuk membuat Line News seperti yang bisa kita lihat di situs berita online, Untuk Stop Marquee Silahkan Hover Tulisan Ini.. !</marquee>
+</body>
+</html>
+uee>
