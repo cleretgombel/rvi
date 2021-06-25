b531969403744aa8060d890753d440d2a7e45e5a
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-25 10:30:45 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<br /><html><head></head><body><teamtalk>
+	<host>
+		<name>R.V.I</name>
+		<address>103.30.246.75</address>
+		<tcpport>10333</tcpport>
+		<udpport>10333</udpport>
+		<encrypted>false</encrypted>
+		<auth>
+			<username>user-rvi</username>
+			<password>virtualradio</password>
+		</auth>
+		<join><channel>/ch 001/</channel><password></password></join>
+	</host>
+</teamtalk>
+</body></html>
