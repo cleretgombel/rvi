c074c04a3fd8d28b4e6ac14249746c45b1a9d3b3
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-25 10:36:44 +0700

    Update myserver.php

--- .github/workflows/myserver.php
+++ .github/workflows/myserver.php
@@ -1,36 +1,16 @@
-# This is a basic workflow to help you get started with Actions
-
-name: CI
-
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the main branch
-  push:
-    branches: [ main ]
-  pull_request:
-    branches: [ main ]
-
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
-
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
-
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v2
-
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
-
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+<!DOCTYPE html>
+<br /><html><head></head><body><teamtalk>
+	<host>
+		<name>R.V.I</name>
+		<address>103.30.246.75</address>
+		<tcpport>10333</tcpport>
+		<udpport>10333</udpport>
+		<encrypted>false</encrypted>
+		<auth>
+			<username>user-rvi</username>
+			<password>virtualradio</password>
+		</auth>
+		<join><channel>/ch 001/</channel><password></password></join>
+	</host>
+</teamtalk>
+</body></html>
